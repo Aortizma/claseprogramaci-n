9e015da98a2f90479cab5a559b878c68c8dac9c6
Aortizma <53451592+Aortizma@users.noreply.github.com>
2019-07-29 17:09:22 -0500

    Add files via upload

---
+++ miprimerpy.py
@@ -0,0 +1,32 @@
+'''Description: el robot debe llevar una banana hasta la casilla 5 tomar la manzana y dejar la banana por ultimo volver a casa" 
+#autor: Andrea Ortiz  
+#fecha:29-07-2019'''
+carries_object("banana")
+print("Hola soy reeborg")
+print("voy a cumplir con mi misión")
+move()
+move()
+move()
+move()
+take("apple")
+#pause()
+put ("banana")
+#pause()
+turn_left()
+turn_left()
+#pause()
+move()
+move()
+move()
+move()
+
+
+
+
+    
+
+
+    
+    
+    
+
